f259bfc87ab05e6ef721239e4381bd1ae4319101
Justin Phillips <justin_phillips@ultimatesoftware.com>
2017-08-04 10:16:57 -0400

    Fixed test name that was breaking build for accounts.

--- accounts/src/test/java/ultimatesoftware/banking/accounts/AccountsApplicationTests.java
+++ accounts/src/test/java/ultimatesoftware/banking/accounts/AccountsApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class CustomersApplicationTests {
+public class AccountsApplicationTests {
 
 	@Test
 	public void contextLoads() {
