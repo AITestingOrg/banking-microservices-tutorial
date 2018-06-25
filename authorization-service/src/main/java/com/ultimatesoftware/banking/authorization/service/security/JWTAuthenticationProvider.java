fcb19974cb21e664ab197bc67ae553cc96fb64a5
Marvin <contact@marvinamari.com>
2018-06-25 17:14:48 -0400

    Validate user credentials

--- authorization-service/src/main/java/com/ultimatesoftware/banking/authorization/service/security/JWTAuthenticationProvider.java
+++ authorization-service/src/main/java/com/ultimatesoftware/banking/authorization/service/security/JWTAuthenticationProvider.java
@@ -1,28 +1,23 @@
 package com.ultimatesoftware.banking.authorization.service.security;
 
-import com.ultimatesoftware.banking.authorization.service.User.ServiceUserRepository;
-import com.ultimatesoftware.banking.authorization.service.model.JwtAuthenticationToken;
-import com.ultimatesoftware.banking.authorization.service.model.User;
-import com.ultimatesoftware.banking.authorization.service.model.JwtUserDetails;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.authentication.AuthenticationProvider;
-import org.springframework.security.authentication.BadCredentialsException;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.authentication.dao.AbstractUserDetailsAuthenticationProvider;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.AuthenticationException;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.AuthorityUtils;
-import org.springframework.security.core.userdetails.UserDetails;
+import com.ultimatesoftware.banking.authorization.service.model.JwtAuthenticationToken;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import com.ultimatesoftware.banking.authorization.service.User.ServiceUserRepository;
+import com.ultimatesoftware.banking.authorization.service.model.JwtUserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import com.ultimatesoftware.banking.authorization.service.model.User;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.stereotype.Component;
 
-import java.util.ArrayList;
 import java.util.List;
 
-//this is authentication
 @Component
-public class JWTAuthorizationFilter extends AbstractUserDetailsAuthenticationProvider {
+public class JWTAuthenticationProvider extends AbstractUserDetailsAuthenticationProvider {
 
     @Autowired
     private JwtValidator validator;
@@ -35,15 +30,6 @@ public class JWTAuthorizationFilter extends AbstractUserDetailsAuthenticationPro
 
     @Override
     protected void additionalAuthenticationChecks(UserDetails userDetails, UsernamePasswordAuthenticationToken token) throws AuthenticationException {
-//
-//        if(token.getCredentials() == null
-//                || userDetails.getPassword() == null) {
-//            throw new BadCredentialsException("Credentials may not be null.");
-//        }
-//
-//        if(!encoder.matches((String) token.getCredentials(),userDetails.getPassword())) {
-//            throw new BadCredentialsException("Invalid Password!");
-//        }
     }
 
     @Override
@@ -57,18 +43,16 @@ public class JWTAuthorizationFilter extends AbstractUserDetailsAuthenticationPro
         if(user == null) {
             throw new RuntimeException("JWT Token is incorrect");
         }
-        //else if ()
 
-      //  if(userRepository.exists(user.getId())) {
+        else if (userRepository.exists(user.getId())) {
             List<GrantedAuthority> grantedAuthorities = AuthorityUtils
                     .commaSeparatedStringToAuthorityList(user.getRole());
             return new JwtUserDetails(user.getUserName(), user.getId(),
                     token,
                     grantedAuthorities);
-      //  }
-     //   else throw new RuntimeException("User was not found");
+        }
+        else throw new RuntimeException("User was not found");
     }
-
 
     @Override
     public boolean supports(Class<?> aClass) {
