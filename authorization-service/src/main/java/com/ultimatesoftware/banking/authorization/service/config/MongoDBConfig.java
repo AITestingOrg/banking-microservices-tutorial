11731055af6bee5db82cb40f30347eb83e9f9a94
Marvin <contact@marvinamari.com>
2018-06-25 17:10:02 -0400

    Mongo config

--- authorization-service/src/main/java/com/ultimatesoftware/banking/authorization/service/config/MongoDBConfig.java
+++ authorization-service/src/main/java/com/ultimatesoftware/banking/authorization/service/config/MongoDBConfig.java
@@ -1,4 +1,13 @@
 package com.ultimatesoftware.banking.authorization.service.config;
 
+import com.ultimatesoftware.banking.authorization.service.User.ServiceUserRepository;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+import org.springframework.context.annotation.Configuration;
+
+@EnableMongoRepositories(basePackageClasses = ServiceUserRepository.class)
+@Configuration
 public class MongoDBConfig {
+
+
 }
+
