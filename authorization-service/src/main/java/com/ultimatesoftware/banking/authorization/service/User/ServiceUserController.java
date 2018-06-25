90c191a37b26ad9435a2a3da354ac2f8f0c31bf4
Marvin <contact@marvinamari.com>
2018-06-25 17:07:22 -0400

    Controller to Register and Login for access token

--- authorization-service/src/main/java/com/ultimatesoftware/banking/authorization/service/User/ServiceUserController.java
+++ authorization-service/src/main/java/com/ultimatesoftware/banking/authorization/service/User/ServiceUserController.java
@@ -1,24 +1,67 @@
 package com.ultimatesoftware.banking.authorization.service.User;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import com.ultimatesoftware.banking.authorization.service.security.JwtGenerator;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.ultimatesoftware.banking.authorization.service.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.HttpStatus;
+
 
 @RestController
-@RequestMapping(value="authapi/v1/users")
+@RequestMapping(value="api/v1/auth")
 public class ServiceUserController {
 
-    private ServiceUserRepository serviceUserRepository;
-
     @Autowired
+    private ServiceUserRepository serviceUserRepository;
     private BCryptPasswordEncoder bCryptPasswordEncoder;
+    @Autowired
+    private JwtGenerator generator;
 
-    @PostMapping
-    public void register(@RequestBody ServiceUser serviceUser) {
-        serviceUser.setPassword(bCryptPasswordEncoder.encode(serviceUser.getPassword()));
-        serviceUserRepository.save(serviceUser);
+    public ServiceUserController(ServiceUserRepository serviceUserRepository,
+                          BCryptPasswordEncoder bCryptPasswordEncoder,
+                                 JwtGenerator generator) {
+        this.serviceUserRepository = serviceUserRepository;
+        this.bCryptPasswordEncoder = bCryptPasswordEncoder;
+        this.generator = generator;
+    }
+
+    @PostMapping(value="register")
+    public ResponseEntity register(@RequestBody User userObj) {
+        if(userObj.getUserName() == null || userObj.getUserName().isEmpty()) {
+            throw new BadCredentialsException("userName parameter required");}
+        if(userObj.getPassword() == null || userObj.getPassword().isEmpty()) {
+            throw new BadCredentialsException("userName parameter required");}
+        User user = new User(userObj.getUserName(),userObj.getPassword());
+        user.setPassword(bCryptPasswordEncoder.encode(userObj.getPassword()));
+        serviceUserRepository.save(user);
+        User useract = new User(userObj.getUserName(),userObj.getPassword());
+        serviceUserRepository.save(useract);
+
+        String account = "{ \"id\": \"" + useract.getId() + "\",\n\"userName\": \"" + useract.getUserName()
+                + "\",\n\"password\": insertYourPassword\n}";
+
+        return new ResponseEntity<>(account, HttpStatus.ACCEPTED);
+    }
+
+    @PostMapping(value="login")
+    public ResponseEntity login(@RequestBody User userObj) {
+
+       User userRecord = serviceUserRepository.findOne(userObj.getId());
+        if(bCryptPasswordEncoder.matches(userObj.getPassword(), userRecord.getPassword())) {
+            return new ResponseEntity<>(generator.generate(userRecord), HttpStatus.ACCEPTED);
+        }
+        if(userObj.getId().equals(userRecord.getId()) && userObj.getPassword().equalsIgnoreCase(userRecord.getPassword()) && userObj.getUserName().equalsIgnoreCase(userRecord.getUserName())) {
+            return new ResponseEntity<>(generator.generate(userRecord), HttpStatus.ACCEPTED);
+        }
+
+        return new ResponseEntity<>("User Not Found", HttpStatus.ACCEPTED);
+    }
+
+    @GetMapping(value="test")
+    public String hello() {
+        return "Hello World";
     }
 }
