3cbdb0396c9167f1c8d983c7c8a4dbc920ce03e0
Marvin <contact@marvinamari.com>
2018-06-25 17:15:45 -0400

    Generate and validate JWT tokens

--- authorization-service/src/main/java/com/ultimatesoftware/banking/authorization/service/security/JWTValidator.java
+++ authorization-service/src/main/java/com/ultimatesoftware/banking/authorization/service/security/JWTValidator.java
@@ -1,4 +1,37 @@
 package com.ultimatesoftware.banking.authorization.service.security;
 
-public class JWTValidator {
+import com.ultimatesoftware.banking.authorization.service.model.User;
+import org.springframework.stereotype.Component;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+
+import java.util.UUID;
+
+import static com.ultimatesoftware.banking.authorization.service.security.SecurityConstants.SECRET;
+
+@Component
+public class JwtValidator {
+
+
+    public User validate(String token) {
+
+        User user = null;
+        try {
+            Claims body = Jwts.parser()
+                    .setSigningKey(SECRET)
+                    .parseClaimsJws(token)
+                    .getBody();
+
+            user = new User();
+
+            user.setUserName(body.getSubject());
+            user.setId(UUID.fromString((String)body.get("userId")));
+            user.setRole((String) body.get("role"));
+        }
+        catch (Exception e) {
+            System.out.println(e);
+        }
+
+        return user;
+    }
 }
--- authorization-service/src/main/java/com/ultimatesoftware/banking/authorization/service/security/JwtGenerator.java
+++ authorization-service/src/main/java/com/ultimatesoftware/banking/authorization/service/security/JwtGenerator.java
@@ -1,4 +1,31 @@
 package com.ultimatesoftware.banking.authorization.service.security;
 
+import com.ultimatesoftware.banking.authorization.service.model.User;
+import org.springframework.stereotype.Component;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import static com.ultimatesoftware.banking.authorization.service.security.SecurityConstants.SECRET;
+
+
+@Component
 public class JwtGenerator {
+
+
+    public String generate(User user) {
+
+
+        Claims claims = Jwts.claims()
+
+                .setSubject(user.getUserName());
+        claims.put("userId", String.valueOf(user.getId()));
+        claims.put("role", user.getRole());
+
+
+        return Jwts.builder()
+                .setClaims(claims)
+                .signWith(SignatureAlgorithm.HS512, SECRET)
+                .compact();
+    }
 }
+
