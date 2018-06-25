5f291a0b5c11bda8fc073d1506843a1d95c4eed0
Marvin <contact@marvinamari.com>
2018-06-25 17:05:58 -0400

    Create base class for Spring Boot

--- authorization-service/src/main/java/com/ultimatesoftware/banking/authorization/service/AuthorizationService.java
+++ authorization-service/src/main/java/com/ultimatesoftware/banking/authorization/service/AuthorizationService.java
@@ -1,4 +1,19 @@
 package com.ultimatesoftware.banking.authorization.service;
 
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.boot.SpringApplication;
+
+@SpringBootApplication
 public class AuthorizationService {
+
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuthorizationService.class, args);
+    }
 }
