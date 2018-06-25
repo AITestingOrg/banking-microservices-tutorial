4dbd5d7c22a8fc0367afab640f18efb2d92b6075
Marvin <contact@marvinamari.com>
2018-06-25 17:12:20 -0400

    Failed authentication entry point

--- authorization-service/src/main/java/com/ultimatesoftware/banking/authorization/service/security/JWTAuthenticationEntryPoint.java
+++ authorization-service/src/main/java/com/ultimatesoftware/banking/authorization/service/security/JWTAuthenticationEntryPoint.java
@@ -1,4 +1,18 @@
 package com.ultimatesoftware.banking.authorization.service.security;
 
-public class JWTAuthenticationEntryPoint {
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.ServletException;
+import java.io.IOException;
+
+@Component
+public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
+    @Override
+    public void commence(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, AuthenticationException e) throws IOException, ServletException {
+
+        httpServletResponse.sendError(HttpServletResponse.SC_UNAUTHORIZED, e.getMessage());
+    }
 }
