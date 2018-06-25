61264abad2089401b8cf8d438c273a2d6bd82c7e
Marvin <contact@marvinamari.com>
2018-06-25 17:13:21 -0400

    User details object for passing credentials

--- authorization-service/src/main/java/com/ultimatesoftware/banking/authorization/service/model/JwtUserDetails.java
+++ authorization-service/src/main/java/com/ultimatesoftware/banking/authorization/service/model/JwtUserDetails.java
@@ -1,29 +1,71 @@
-package com.ultimatesoftware.banking.authorization.service.User;
+package com.ultimatesoftware.banking.authorization.service.model;
 
-import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.stereotype.Service;
+import org.springframework.security.core.GrantedAuthority;
+import java.util.Collection;
+import java.util.List;
+import java.util.*;
 
-import static java.util.Collections.emptyList;
+public class JwtUserDetails implements UserDetails {
 
-@Service
-public class UserDetailsServiceImpl implements UserDetailsService {
-    private ServiceUserRepository serviceUserRepository;
+    private String userName;
+    private String token;
+    private UUID id;
+    private Collection<? extends GrantedAuthority> authorities;
 
 
-    public UserDetailsServiceImpl(ServiceUserRepository serviceUserRepository) {
-        this.serviceUserRepository = serviceUserRepository;
+    public JwtUserDetails(String userName, UUID id, String token, List<GrantedAuthority> grantedAuthorities) {
+
+        this.userName = userName;
+        this.id = id;
+        this.token= token;
+        this.authorities = grantedAuthorities;
     }
-    public UserDetailsServiceImpl(){}
 
     @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        ServiceUser serviceUser = serviceUserRepository.findByUsername(username);
-        if (serviceUser == null) {
-            throw new UsernameNotFoundException(username);
-        }
-        return new User(serviceUser.getUsername(), serviceUser.getPassword(), emptyList());
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return authorities;
     }
+
+    @Override
+    public String getPassword() {
+        return null;
+    }
+
+    @Override
+    public String getUsername() {
+        return userName;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+
+    public String getToken() {
+        return token;
+    }
+
+
+    public UUID getId() {
+        return id;
+    }
+
 }
+
