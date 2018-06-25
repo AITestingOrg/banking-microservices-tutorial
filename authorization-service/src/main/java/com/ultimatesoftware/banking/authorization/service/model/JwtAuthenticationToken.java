3f350dfb110003104edd46f148882c0fa315894b
Marvin <contact@marvinamari.com>
2018-06-25 17:11:51 -0400

    Custom UserPassWord authentication Token

--- authorization-service/src/main/java/com/ultimatesoftware/banking/authorization/service/model/JwtAuthenticationToken.java
+++ authorization-service/src/main/java/com/ultimatesoftware/banking/authorization/service/model/JwtAuthenticationToken.java
@@ -1,4 +1,40 @@
 package com.ultimatesoftware.banking.authorization.service.model;
 
-public class JWTAuthenticationToken {
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+
+
+public class JwtAuthenticationToken extends UsernamePasswordAuthenticationToken{
+
+    private String token;
+
+
+    public JwtAuthenticationToken(String token) {
+        super(null, null);
+        this.token = token;
+    }
+    public JwtAuthenticationToken(String token, User user) {
+        super(user, null, null);
+        this.token = token;
+    }
+
+//public JwtAuthenticationToken(String token, Object principal, Object credentials, Collection<? extends GrantedAuthority> authority){super(principal,credentials,authority);}
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+    @Override
+    public Object getCredentials() {
+        return null;
+    }
+
+    @Override
+    public Object getPrincipal() {
+        return null;
+    }
+
 }
