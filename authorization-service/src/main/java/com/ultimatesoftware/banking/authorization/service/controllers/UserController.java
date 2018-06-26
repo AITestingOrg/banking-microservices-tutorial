331830cc288d3840d23820a13d7026e335c841ee
Marvin <contact@marvinamari.com>
2018-06-26 01:18:55 -0400

    Refactor logic from UserController into AuthenticationService

--- authorization-service/src/main/java/com/ultimatesoftware/banking/authorization/service/controllers/UserController.java
+++ authorization-service/src/main/java/com/ultimatesoftware/banking/authorization/service/controllers/UserController.java
@@ -1,13 +1,9 @@
 package com.ultimatesoftware.banking.authorization.service.controllers;
 
 import com.ultimatesoftware.banking.authorization.service.model.User;
-import com.ultimatesoftware.banking.authorization.service.security.JWTGenerator;
-import com.ultimatesoftware.banking.authorization.service.services.ServiceUserRepository;
+import com.ultimatesoftware.banking.authorization.service.services.AuthenticationService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.BadCredentialsException;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
 
@@ -16,49 +12,16 @@ import org.springframework.web.bind.annotation.*;
 public class UserController {
 
     @Autowired
-    private ServiceUserRepository serviceUserRepository;
-    private BCryptPasswordEncoder bCryptPasswordEncoder;
-    @Autowired
-    private JWTGenerator generator;
-
-    public UserController(ServiceUserRepository serviceUserRepository,
-                          BCryptPasswordEncoder bCryptPasswordEncoder,
-                          JWTGenerator generator) {
-        this.serviceUserRepository = serviceUserRepository;
-        this.bCryptPasswordEncoder = bCryptPasswordEncoder;
-        this.generator = generator;
-    }
+    private AuthenticationService service;
 
     @PostMapping(value="register")
-    public ResponseEntity register(@RequestBody User userObj) {
-        if(userObj.getUserName() == null || userObj.getUserName().isEmpty()) {
-            throw new BadCredentialsException("userName parameter required");}
-        if(userObj.getPassword() == null || userObj.getPassword().isEmpty()) {
-            throw new BadCredentialsException("userName parameter required");}
-        User user = new User(userObj.getUserName(),userObj.getPassword());
-        user.setPassword(bCryptPasswordEncoder.encode(userObj.getPassword()));
-        serviceUserRepository.save(user);
-        User useract = new User(userObj.getUserName(),userObj.getPassword());
-        serviceUserRepository.save(useract);
-
-        String account = "{ \"id\": \"" + useract.getId() + "\",\n\"userName\": \"" + useract.getUserName()
-                + "\",\n\"password\": insertYourPassword\n}";
-
-        return new ResponseEntity<>(account, HttpStatus.ACCEPTED);
+    public ResponseEntity register(@RequestBody User user) {
+        return service.createUser(user);
     }
 
     @PostMapping(value="login")
-    public ResponseEntity login(@RequestBody User userObj) {
-
-       User userRecord = serviceUserRepository.findOne(userObj.getId());
-        if(bCryptPasswordEncoder.matches(userObj.getPassword(), userRecord.getPassword())) {
-            return new ResponseEntity<>(generator.generate(userRecord), HttpStatus.ACCEPTED);
-        }
-        if(userObj.getId().equals(userRecord.getId()) && userObj.getPassword().equalsIgnoreCase(userRecord.getPassword()) && userObj.getUserName().equalsIgnoreCase(userRecord.getUserName())) {
-            return new ResponseEntity<>(generator.generate(userRecord), HttpStatus.ACCEPTED);
-        }
-
-        return new ResponseEntity<>("User Not Found", HttpStatus.ACCEPTED);
+    public ResponseEntity login(@RequestBody User user) {
+        return service.authenticateUser(user);
     }
 
     @GetMapping(value="test")
