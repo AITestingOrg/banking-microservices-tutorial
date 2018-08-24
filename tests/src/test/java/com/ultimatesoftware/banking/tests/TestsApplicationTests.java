980624f15cf1a0599ebec6b35a77688bc1eb627b
Justin Phillips <euangelion3o9@gmail.com>
2018-08-24 16:41:08 -0400

    Added tests project.

---
+++ tests/src/test/java/com/ultimatesoftware/banking/tests/TestsApplicationTests.java
@@ -0,0 +1,16 @@
+package com.ultimatesoftware.banking.tests;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TestsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
