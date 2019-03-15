e0c2630cea61b51f2221c2144d4f37b9f826df5a
Justin Phillips <justin_phillips@ultimatesoftware.com>
2019-03-15 18:33:07 -0400

    Fixed test execution

--- scripts/run-unit-test.sh
+++ scripts/run-unit-test.sh
@@ -1,4 +1,4 @@
 '#!/bin/sh'
-../gradlew cleanTest :transactions:test --tests "*.unit.*"
-../gradlew :transactions:jacocoTestCoverageVerification
-../gradlew :transactions:jacocoTestReport
+./gradlew test --tests "*.unit.*"
+./gradlew :transactions:jacocoTestCoverageVerification
+./gradlew :transactions:jacocoTestReport
